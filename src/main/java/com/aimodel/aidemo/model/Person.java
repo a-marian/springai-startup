8a6b10160249c803b11ba420c6538bf64dcc9be3
marelas <marela.as@gmail.com>
2025-05-01 17:11:40 -0300

    initial

---
+++ src/main/java/com/aimodel/aidemo/model/Person.java
@@ -0,0 +1,16 @@
+package com.aimodel.aidemo.model;
+
+public class Person {
+
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private Gender gender;
+    private String nationality;
+
+    //... GETTERS/SETTERS
+}
+ enum Gender {
+    MALE, FEMALE;
+}
